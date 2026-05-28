8974a4187ab6523a1b868170420e72438401553d
Andrew <gus123tractor@gmail.com>
2026-05-28 20:13:41 +0400

    specify packages list backup directory

--- backup-packages.fish
+++ backup-packages.fish
@@ -1,17 +1,18 @@
 #!/usr/bin/env fish
 
-set backup_dir /sdcard/Termux/Backups
-
 function usage
     echo "Usage: fish backup-packages.fish [backup|restore]"
-    echo "  backup           - create new backup"
+    echo "  backup  <dir>    - create new backup"
     echo "  restore <file>   - restore from archive"
     exit 1
 end
 
 switch $argv[1]
     case backup
-        dpkg --get-selections | grep -v deinstall | awk '{print $1}' > /sdcard/Termux/Backups/packages_(date +%Y_%m_%d_%H-%M).txt
+			  if test -z "$argv[2]"
+            usage
+        end
+        dpkg --get-selections | grep -v deinstall | awk '{print $1}' > $argv[2]/packages_(date +%Y_%m_%d_%H-%M).txt
     case restore
         if test -z "$argv[2]"
             usage
