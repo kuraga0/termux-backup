0f97d3b9de488f0eec192b3554eedc3fbdffdd9d
Andrew <gus123tractor@gmail.com>
2026-05-28 17:12:35 +0400

    Initial commit

---
+++ backup-packages.fish
@@ -0,0 +1,28 @@
+#!/usr/bin/env fish
+
+set backup_dir /sdcard/Termux/Backups
+
+function usage
+    echo "Usage: backup-packages.fish [backup|restore]"
+    echo "  backup           - create new backup"
+    echo "  restore <file>   - restore from archive"
+    exit 1
+end
+
+switch $argv[1]
+    case backup
+        dpkg --get-selections | grep -v deinstall | awk '{print $1}' > /sdcard/Termux/Backups/packages_(date +%Y_%m_%d_%H-%M).txt
+    case restore
+        if test -z "$argv[2]"
+            usage
+        end
+        set archive $argv[2]
+        if not test -f $archive
+            echo "File not found: $archive"
+            exit 1
+        end
+        xargs apt install -y < $archive
+
+    case '*'
+        usage
+end
